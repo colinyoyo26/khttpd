639e24745b11e91c468d55884d2253bc343f2785
colinyoyo26 <colinyoyo26@gmail.com>
2020-04-06 17:27:35 +0800

    Rewrite kHTTPd with CMWQ
    Solve bad RIP value issue after unload module.
    Not yet quantify the performance.

--- http_server.h
+++ http_server.h
@@ -7,6 +7,17 @@ struct http_server_param {
     struct socket *listen_socket;
 };
 
+struct khttp_service {
+    bool is_stopped;
+    struct list_head worker;
+};
+
+struct khttp_worker {
+    struct socket *sock;
+    struct list_head list;
+    struct work_struct khttp_work;
+};
+
 extern int http_server_daemon(void *arg);
 char *fib_sequence(long long k);
 
